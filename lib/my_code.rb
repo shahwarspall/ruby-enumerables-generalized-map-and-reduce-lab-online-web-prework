be0948e5b4cd056f68f819541ced43726b6f787b
Shahwar Spall <shevy.spall@gmail.com>
2019-07-09 04:58:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,11 +10,17 @@ end
 
 
 def reduce(array,source=nil)
-  new = []
-  i = 0 
-  while i < array.length do 
-    new.push (yield(array[i]))
-    i += 1 
+  if source
+    sum = source
+    i = 0
+  else
+    sum = array[0]
+    i = 1
   end
-  return new
+
+  while i < array.length
+    sum = yield(sum, array[i])
+    i += 1
+  end
+  sum
 end
